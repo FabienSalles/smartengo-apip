74a96cdd4a5091dda005aaf4dda0a64a5305d606
FabienSalles <blacked19@gmail.com>
2020-11-10 14:34:22 +0100

    add phpstan

--- Makefile-api.mk
+++ Makefile-api.mk
@@ -1,5 +1,10 @@
+DC_EXEC_API_PHP=docker-compose exec php
+
 api/php-cs-check:
 	./bin/api/php-cs-fixer fix --dry-run --diff
 
 api/php-cs-fix:
 	./bin/api/php-cs-fixer fix
+
+api/phpstan:
+	$(DC_EXEC_API_PHP) bin/phpstan analyse --memory-limit=-1
